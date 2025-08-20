ddf255751b8d57a00dcb60904a64d6b6041e222a
sayedhajaj <sayedhajaj@gmail.com>
2025-08-20 11:42:37 +0100

    Update web

--- src/web.ts
+++ src/web.ts
@@ -367,13 +367,13 @@ export class BluetoothLeWeb extends WebPlugin implements BluetoothLePlugin {
       filters.push({
         services: [service],
         name: options?.name,
-        namePrefix: options?.namePrefix,
+        namePrefixes: options?.namePrefixes,
       });
     }
-    if ((options?.name || options?.namePrefix) && filters.length === 0) {
+    if ((options?.name || options?.namePrefixes) && filters.length === 0) {
       filters.push({
         name: options.name,
-        namePrefix: options.namePrefix,
+        namePrefixes: options.namePrefixes,
       });
     }
     for (const manufacturerData of options?.manufacturerData ?? []) {
