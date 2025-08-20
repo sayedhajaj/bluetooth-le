da75cbeeba799630671ee2d928dca68e6e0c2e21
sayedhajaj <sayedhajaj@gmail.com>
2025-08-20 13:48:07 +0100

    Update ios get name prefixes

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -148,7 +148,7 @@ public class BluetoothLe: CAPPlugin {
 
         let serviceUUIDs = self.getServiceUUIDs(call)
         let name = call.getString("name")
-        let namePrefixes = call.getArray("namePrefixes")
+        let namePrefixes = call.getArray("namePrefixes", String.self)
         let allowDuplicates = call.getBool("allowDuplicates", false)
         let manufacturerDataFilters = self.getManufacturerDataFilters(call)
 
